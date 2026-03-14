5b259f561418197763c45534cd84fe6cd16f770e
Igorsky <igorgn@wix.com>
2026-03-14 20:06:15 +0200

    add border to help pop up

--- init.lua
+++ init.lua
@@ -185,6 +185,10 @@ vim.diagnostic.config {
   jump = { float = true },
 }
 
+vim.keymap.set('n', 'K', function()
+  vim.lsp.buf.hover { border = 'rounded' }
+end, { desc = 'Hover Documentation' })
+
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
