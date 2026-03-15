7ff0d92d45d000b3ce28cec8359313ef6ce6841d
Igorsky <igorgn@wix.com>
2026-03-15 14:56:05 +0200

    increase copilot window

--- lua/custom/plugins/copilot_chat.lua
+++ lua/custom/plugins/copilot_chat.lua
@@ -10,7 +10,7 @@ return {
       temperature = 0.1, -- Lower = focused, higher = creative
       window = {
         layout = 'vertical', -- 'vertical', 'horizontal', 'float'
-        width = 0.4, -- 50% of screen width
+        width = 0.7, -- 50% of screen width
       },
       auto_insert_mode = true, -- Enter insert mode when opening
     },
