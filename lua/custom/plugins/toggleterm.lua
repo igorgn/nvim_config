e47e503ed02c0f0770daaca835fd84e28e33715e
Igorsky <igorgn@wix.com>
2026-03-19 15:16:46 +0200

    add claude code

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -6,6 +6,10 @@ return {
       require('toggleterm').setup {
         open_mapping = '<C-\\>',
         direction = 'float',
+        float_opts = {
+          width = function() return math.floor(vim.o.columns * 0.8) end,
+          height = function() return math.floor(vim.o.lines * 0.8) end,
+        },
       }
     end,
   },
